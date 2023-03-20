707dd61a81d5da211aafd8dbea2996d35da420cd
CristopherAreche <cristopherareche764@gmail.com>
2023-03-19 20:18:00 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./app/store.js";
@@ -11,9 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename="/client">
+    <BrowserRouter basename="/pokemon-website">
       <App />
     </BrowserRouter>
   </Provider>
 );
-reportWebVitals();
